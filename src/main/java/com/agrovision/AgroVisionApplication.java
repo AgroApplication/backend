a8920ef6aedb255fed8c753014a63c4d21b58ce3
Anupa200212 <supulanupa@gmail.com>
2026-04-04 00:59:35 +0530

    initialize Spring Boot application with base configuration and H2 database settings

--- src/main/java/com/agrovision/AgroVisionApplication.java
+++ src/main/java/com/agrovision/AgroVisionApplication.java
@@ -2,8 +2,12 @@ package com.agrovision;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.agrovision.entity")
+@EnableJpaRepositories(basePackages = "com.agrovision.repository")
 public class AgroVisionApplication {
 
     public static void main(String[] args) {
